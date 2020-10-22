0475f46d2965bf616723880638203b067ac3ca57
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 22:38:17 +0530

    added getCompoundInterest method in CompoundInterest class

--- basicCalculations/CompoundInterest.java
+++ basicCalculations/CompoundInterest.java
@@ -1,19 +1,23 @@
 public class CompoundInterest{
-  public static void main(String [] args){
-    if(args.length >= 3){
-      int principle = Integer.parseInt(args[0]);
-      double rate = Integer.parseInt(args[1])/100.00;
-      int period = Integer.parseInt(args[2]);
-      double totalAmount = principle;
-      int count = 0;
-      while(count<period){
-        totalAmount = totalAmount * (1+rate);
-        count++; 
-      }
-      double compoundInterest = totalAmount - principle;
-      System.out.println("Compound Interest of principle:" + principle + ", rate:" + rate + ", period:" + period + " is " + compoundInterest);
-    }else{
-      System.out.println("Please provide a valid input");
+  public static double getCompoundInterest(int principle, double rate, int period){
+    double totalAmount = principle;
+
+    for(int count = 0; count < period; count++){
+      totalAmount = totalAmount * (1+rate);
     }
+    return totalAmount - principle;
+  }
+
+  public static void main(String [] args){
+    if(args.length < 3){
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
+    int principle = Integer.parseInt(args[0]);
+    double rate = Integer.parseInt(args[1])/100.00;
+    int period = Integer.parseInt(args[2]);
+    double compoundInterest = getCompoundInterest(principle, rate, period);
+    System.out.println("Compound Interest of principle:" + principle + ", rate:" + rate + ", period:" + period + " is " + compoundInterest);
   }
 }
