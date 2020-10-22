2ca6263dc6140775434261805fdeccc3ada68d2d
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 13:55:14 +0530

    modified CompoundInterest class

--- basics/CompoundInterest.java
+++ basics/CompoundInterest.java
@@ -2,21 +2,17 @@ public class CompoundInterest{
   public static void main(String [] args){
     if(args.length != 0){
       int principle = Integer.parseInt(args[0]);
-      int rate = Integer.parseInt(args[1]);
+      double rate = Integer.parseInt(args[1])/100.00;
       int period = Integer.parseInt(args[2]);
 
-    double compoundPrinciple = principle;
-    int i = 0;
-    while(i<period){
-      compoundPrinciple = compoundPrinciple * (1+rate);
-      i++; 
-    }
-    // for(int i=t; t>0; t--) {
-    //   a = a *(1+r);
-    // }
-
-    double compoundInterest = compoundPrinciple - principle;
-    System.out.println("Compound Interest of principle:" + principle + ", rate:" + rate + ", period:" + period + " is " + compoundInterest);
+      double compoundPrinciple = principle;
+      int count = 0;
+      while(count<period){
+        compoundPrinciple = compoundPrinciple * (1+rate);
+        count++; 
+      }
+      double compoundInterest = compoundPrinciple - principle;
+      System.out.println("Compound Interest of principle:" + principle + ", rate:" + rate + ", period:" + period + " is " + compoundInterest);
     }
   }
 }
