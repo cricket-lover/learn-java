3c7b281c28787a0eb746cd4cfb80800e49245999
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 22:44:08 +0530

    added getCentigrade method in ConvertToCentigrade class

--- basicCalculations/ConvertToCentigrade.java
+++ basicCalculations/ConvertToCentigrade.java
@@ -1,11 +1,15 @@
 public class ConvertToCentigrade {
+  public static double getCentigrade(double fahrenheit){
+    return (fahrenheit - 32) * 5/9;
+  }
+
   public static void main(String[] args){
-    if(args.length != 0){
-      double fahrenheit = Float.parseFloat(args[0]);
-      double centigrade = (fahrenheit - 32) * 5/9;
-      System.out.println("Temperature of " + fahrenheit + " degrees fahrenheit in centigrade is " +  centigrade);
-    }else{
-      System.out.println("Please provide a valid input");
+    if(args.length == 0){
+      System.out.println("Insufficient arguments");
+      return;
     }
+
+    double fahrenheit = Float.parseFloat(args[0]);
+    System.out.println("Temperature of " + fahrenheit + " degrees fahrenheit in centigrade is " +  getCentigrade(fahrenheit));
   }
 }
