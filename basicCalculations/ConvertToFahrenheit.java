f3bcf5265fbe6f78e9a0f42a31f8d35679a1d123
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 22:48:18 +0530

    added getFahrenheit method in ConvertToFahrenheit class

--- basicCalculations/ConvertToFahrenheit.java
+++ basicCalculations/ConvertToFahrenheit.java
@@ -1,11 +1,15 @@
 public class ConvertToFahrenheit{
+  public static double getFahrenheit(double centigrade){
+    return (centigrade * 9/5.00) + 32;
+  }
+
   public static void main(String[] args){
-    if(args.length != 0){
-      double centigrade = Float.parseFloat(args[0]);
-      double fahrenheit = (centigrade * 9/5.00) + 32;
-      System.out.println("Temperature of " + centigrade + " degrees centigrade in fahrenheit is " +  fahrenheit);
-    }else{
-      System.out.println("Please provide a valid input");
+    if(args.length == 0){
+      System.out.println("Insufficient arguments");
+      return;
     }
+    
+    double centigrade = Float.parseFloat(args[0]);
+    System.out.println("Temperature of " + centigrade + " degrees centigrade in fahrenheit is " + getFahrenheit(centigrade));
   }
 }
