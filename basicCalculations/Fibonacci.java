4f1613122e6202fe9ea003c1d402d1c16316925b
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 21:06:02 +0530

    added printFibonacci method in Fibonacci class

--- basicCalculations/Fibonacci.java
+++ basicCalculations/Fibonacci.java
@@ -1,25 +1,23 @@
 public class Fibonacci{
-  public static void main(String [] args){
-    if(args.length != 0){
-      int num = Integer.parseInt(args[0]);
-      int prev = 0;
-      int curr = 1;
-      for(int i=0;i<=num;i++){
-      if(i == 1){
-        System.out.println(prev);
-      }
-      if(i == 2){
-        System.out.println(curr);
-      }
-      if(i>2){
-        int next = prev+curr;
-        System.out.println(next);
-          prev = curr;
-          curr = next;
-        }
-      }
-    }else{
-      System.out.println("Please provide a valid input");
+
+  public static void printFibonacci(int limit){
+    int currentTerm = 0;
+    int nextTerm = 1;
+
+    for(int term=0; term<limit; term++){
+      System.out.println(currentTerm);
+      nextTerm = currentTerm + nextTerm;;
+      currentTerm = nextTerm - currentTerm;
     }
+  }
+
+  public static void main(String [] args){
+    if(args.length == 0){
+      System.out.println("Please provide a valid input");
+      return;
+    }
+
+    int limit = Integer.parseInt(args[0]);
+    printFibonacci(limit);
   }
 }
