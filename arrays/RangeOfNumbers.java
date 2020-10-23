8f000322c26a1ebc977bbca70bec94e9d5199281
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-23 19:35:13 +0530

    Added RangeOfNumbers class
    Added range and displayRangeOfNumbers methods which gives range of numbers.
    Added questions.txt to .gitignore

---
+++ arrays/RangeOfNumbers.java
@@ -0,0 +1,31 @@
+public class RangeOfNumbers {
+
+  public static void displayRangeOfNum(int[] numbers) {
+    for (int currentNumber : numbers) {
+      System.out.println(currentNumber);
+    }
+  }
+
+  public static int[] range(int from, int to, int step) {
+    int[] rangeOfNum = new int[to - from + 1];
+
+    for (int index = 0; index <= rangeOfNum.length - 1; index++) {
+      rangeOfNum[index] = index + from;
+    }
+    return rangeOfNum;
+  }
+
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
+    int from = Integer.parseInt(args[0]);
+    int to = Integer.parseInt(args[1]);
+    int step = args.length == 3 ? Integer.parseInt(args[2]) : 1;
+    int[] rangeOfNum = range(from, to, step);
+
+    displayRangeOfNum(rangeOfNum);
+  }
+}
