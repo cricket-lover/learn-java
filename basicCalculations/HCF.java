c34979daa82e32d39ed7ceb92808c10d209ff514
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-23 08:38:27 +0530

    added getHcf method in HCF class

--- basicCalculations/HCF.java
+++ basicCalculations/HCF.java
@@ -1,20 +1,26 @@
 public class HCF{
-  public static void main(String [] args){
-    if(args.length >= 2){
-      int num1 = Integer.parseInt(args[0]);
-      int num2 = Integer.parseInt(args[1]);
-      int divisor = num1;
-      int dividend = num2;
-      int remainder;
-      while(divisor > 0)
-      {
-        remainder = dividend%divisor;
-        dividend = divisor;
-        divisor = remainder;
-      }
-      System.out.println("GCD of " + num1 + "," + num2 + " is " + dividend);
-    }else{
-      System.out.println("Please provide a valid input");
+  public static int getHcf(int num1, int num2){
+    int divisor = num1;
+    int dividend = num2;
+    int remainder;
+
+    while(divisor > 0)
+    {
+      remainder = dividend % divisor;
+      dividend = divisor;
+      divisor = remainder;
     }
+    return dividend;
+  }
+
+  public static void main(String [] args){
+    if(args.length < 2){
+      System.out.println("Insufficient arguments");
+      return;
+    }
+
+    int num1 = Integer.parseInt(args[0]);
+    int num2 = Integer.parseInt(args[1]); 
+    System.out.println("GCD of " + num1 + "," + num2 + " is " + getHcf(num1, num2));
   }
 }
