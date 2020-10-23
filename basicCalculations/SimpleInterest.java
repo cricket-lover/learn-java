2371aa3d2e3f79d84d582edc798d0269ebe12de0
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-23 09:13:20 +0530

    added getSimpleInterest method in SimpleInterestclass

--- basicCalculations/SimpleInterest.java
+++ basicCalculations/SimpleInterest.java
@@ -1,13 +1,16 @@
 public class SimpleInterest{
+  public static double getSimpleInterest(int principle, int rate, int period){
+    return (principle * rate * period) / 100.00;
+  }
+
   public static void main(String [] args){
-    if(args.length >= 3){
-      int principle = Integer.parseInt(args[0]);
-      int rate = Integer.parseInt(args[1]);
-      int period = Integer.parseInt(args[2]);
-      double interest = (principle*rate*period)/100.00;
-      System.out.println(interest);
-    }else{
-      System.out.println("Please provide a valid input");
+    if(args.length < 3){
+      System.out.println("Insufficient arguments");
     }
+
+    int principle = Integer.parseInt(args[0]);
+    int rate = Integer.parseInt(args[1]);
+    int period = Integer.parseInt(args[2]);
+    System.out.println("Simple Interest of principle:" + principle + ", rate:" + rate + ", period:" + period + " is " + getSimpleInterest(principle, rate, period));
   }
 }
